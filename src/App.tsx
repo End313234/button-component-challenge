c1d3784b3899d7a71c26f9c73e9dd836a086d79e
End313234 <end313234@protonmail.com>
2022-05-07 00:11:52 -0300

    Add more previews

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import styles from "./styles/app.module.scss";
 import { CustomizableButton } from "./web/components/customizable-button";
 
 const App = () => {
-	return <CustomizableButton />;
+	return (
+		<div className={styles.container}>
+			<CustomizableButton />
+			<CustomizableButton variant="text" text="Text variant" />
+			<CustomizableButton variant="outline" text="Outline variant" />
+			<CustomizableButton disableShadow text="Shadow disabled" />
+			<CustomizableButton disabled text="Button disabled" />
+			<CustomizableButton
+				variant="text"
+				text="Button disabled & variant text"
+			/>
+			<CustomizableButton startIcon text="Start icon" />
+			<CustomizableButton endIcon text="End icon" />
+			<CustomizableButton size="sm" text="Small" />
+			<CustomizableButton size="md" text="Medium" />
+			<CustomizableButton size="lg" text="Large" />
+			<CustomizableButton color="primary" text="Primary color" />
+			<CustomizableButton color="secondary" text="Secondary color" />
+			<CustomizableButton color="danger" text="Danger color" />
+		</div>
+	);
 };
 
 export default App;
